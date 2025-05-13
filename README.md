15f98b2b1f499363e44a0370f432d4536fd2ae9e
unknown <Neeck7@yandex.ru>
2025-05-13 03:15:58 +0300

    mesto

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Проектная работа Mesto
 
+https://github.com/Neeck7/mesto-project-ff.git
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,24 @@
-// @todo: Темплейт карточки
+const addCard = (card, deleteCard) => {
+  const cardTemplate = document.querySelector('#card-template').content;
+  const placesItem = cardTemplate.querySelector('.places__item').cloneNode(true);
+  const cardImage = placesItem.querySelector('.card__image');
+  cardImage.src = card.link;
+  cardImage.alt = card.name;
 
-// @todo: DOM узлы
+  placesItem.querySelector('.card__title').textContent = card.name;
+  placesItem.querySelector('.card__delete-button')
+    .addEventListener('click', () => deleteCard(placesItem));
 
-// @todo: Функция создания карточки
+  return placesItem;
+};
 
-// @todo: Функция удаления карточки
+const deleteCard = (placesItem) => {
+  placesItem.remove();
+};
 
-// @todo: Вывести карточки на страницу
+const placesList = document.querySelector('.places__list');
+
+initialCards.forEach((card) => {
+  const cardElement = addCard(card, deleteCard);
+  placesList.append(cardElement);
+});
